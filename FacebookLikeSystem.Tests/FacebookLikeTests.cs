c2fe8c060b6e1755bdc2dc5c11d4c278f3a88dd6
yassine ktiri <yassinektiri@gmail.com>
2022-06-07 23:27:59 +0100

    Finished requirements

--- FacebookLikeSystem.Tests/FacebookLikeTests.cs
+++ FacebookLikeSystem.Tests/FacebookLikeTests.cs
@@ -9,21 +9,25 @@ namespace FacebookLikeSystem.Tests
         {
             Assert.Equal("no one likes this", FacebookLike.CreateLikeText(new string[] { }));
         }
+        
         [Fact]
         public void ShouldHandleOneLike()
         {
             Assert.Equal("Peter likes this", FacebookLike.CreateLikeText(new string[] { "Peter" }));
         }
+        
         [Fact]
         public void ShouldHandleTwoLikes()
         {
             Assert.Equal("Jacob and Alex like this", FacebookLike.CreateLikeText(new string[] { "Jacob", "Alex" }));
         }
+        
         [Fact]
         public void ShouldHandleThreeLikes()
         {
             Assert.Equal("Max, John and Mark like this", FacebookLike.CreateLikeText(new string[] { "Max", "John", "Mark" }));
         }
+        
         [Fact]
         public void ShouldHandleFourLikes()
         {
