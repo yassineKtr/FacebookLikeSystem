6d45357e595edd56513d1334ecb15a719797c091
yassine ktiri <yassinektiri@gmail.com>
2022-06-13 23:06:46 +0100

    Fixed mentioned issues

--- FacebookLikeSystem.Tests/FacebookLikeTests.cs
+++ FacebookLikeSystem.Tests/FacebookLikeTests.cs
@@ -13,25 +13,25 @@ namespace FacebookLikeSystem.Tests
         [Fact]
         public void ShouldHandleOneLike()
         {
-            Assert.Equal("Peter likes this", FacebookLike.CreateLikeText(new string[] { "Peter" }));
+            Assert.Equal("Peter likes this", FacebookLike.CreateLikeText(new [] { "Peter" }));
         }
         
         [Fact]
         public void ShouldHandleTwoLikes()
         {
-            Assert.Equal("Jacob and Alex like this", FacebookLike.CreateLikeText(new string[] { "Jacob", "Alex" }));
+            Assert.Equal("Jacob and Alex like this", FacebookLike.CreateLikeText(new [] { "Jacob", "Alex" }));
         }
         
         [Fact]
         public void ShouldHandleThreeLikes()
         {
-            Assert.Equal("Max, John and Mark like this", FacebookLike.CreateLikeText(new string[] { "Max", "John", "Mark" }));
+            Assert.Equal("Max, John and Mark like this", FacebookLike.CreateLikeText(new [] { "Max", "John", "Mark" }));
         }
         
         [Fact]
         public void ShouldHandleFourLikes()
         {
-            Assert.Equal("Alex, Jacob and 2 others like this", FacebookLike.CreateLikeText(new string[] { "Alex", "Jacob", "Mark", "Max" }));
+            Assert.Equal("Alex, Jacob and 2 others like this", FacebookLike.CreateLikeText(new [] { "Alex", "Jacob", "Mark", "Max" }));
         }
     }
 }
